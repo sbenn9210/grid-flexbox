8a34c12eb1885a0aaeb79c362092e4416a22d209
sbenn9210 <sbenn9210@gmail.com>
2019-06-01 20:43:56 -0500

    initial commit

---
+++ src/Components/Grid.js
@@ -0,0 +1,35 @@
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import React, { Component } from "react";
+
+class PracticeGrid extends Component {
+  render() {
+    return (
+      <div>
+        <Grid container spacing={10}>
+          <Grid item xs={12} md={2} lg={12}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={12}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={12}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={12}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={12}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={12}>
+            <Paper>xs=12</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default PracticeGrid;
