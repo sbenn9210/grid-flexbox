cb61bb80e9f11cd596ee0bfeb3ff067ac277fa39
sbenn9210 <sbenn9210@gmail.com>
2019-06-01 20:49:40 -0500

    second commit

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -7,23 +7,62 @@ class PracticeGrid extends Component {
   render() {
     return (
       <div>
-        <Grid container spacing={10}>
-          <Grid item xs={12} md={2} lg={12}>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={2} lg={3}>
             <Paper>xs=12</Paper>
           </Grid>
-          <Grid item xs={12} md={2} lg={12}>
+          <Grid item xs={12} md={2} lg={3}>
             <Paper>xs=12</Paper>
           </Grid>
-          <Grid item xs={12} md={2} lg={12}>
+          <Grid item xs={12} md={2} lg={3}>
             <Paper>xs=12</Paper>
           </Grid>
-          <Grid item xs={12} md={2} lg={12}>
+          <Grid item xs={12} md={2} lg={3}>
             <Paper>xs=12</Paper>
           </Grid>
-          <Grid item xs={12} md={2} lg={12}>
+          <Grid item xs={12} md={2} lg={4}>
             <Paper>xs=12</Paper>
           </Grid>
-          <Grid item xs={12} md={2} lg={12}>
+          <Grid item xs={12} md={2} lg={4}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={4}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
+            <Paper>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={2} lg={1}>
             <Paper>xs=12</Paper>
           </Grid>
         </Grid>
